0bd88cff4b857f206edcc8324b715816f4a5c9a8
Giulia Titton <68745997+GiuliaTitton@users.noreply.github.com>
2024-06-04 11:08:59 +0200

    Delete STM32project directory

--- STM32project/Debug/Core/Src/no_os_fifo.d
+++
@@ -1,5 +0,0 @@
-Core/Src/no_os_fifo.o: ../Core/Src/no_os_fifo.c ../Core/Inc/no_os_fifo.h \
- ../Core/Inc/no_os_error.h ../Core/Inc/no_os_alloc.h
-../Core/Inc/no_os_fifo.h:
-../Core/Inc/no_os_error.h:
-../Core/Inc/no_os_alloc.h:
--- STM32project/Debug/Core/Src/no_os_util.d
+++
@@ -1,2 +0,0 @@
-Core/Src/no_os_util.o: ../Core/Src/no_os_util.c ../Core/Inc/no_os_util.h
-../Core/Inc/no_os_util.h:
