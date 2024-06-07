a8e729d73c24f1f529789d89f11184da3a86dda1
GiuliaTitton <68745997+GiuliaTitton@users.noreply.github.com>
2024-06-07 18:20:20 +0200

    aggiunti datasheet

--- Arduino Test AD7606/send_uart_data/send_uart_data.ino
+++ Arduino Test AD7606/send_uart_data/send_uart_data.ino
@@ -1,13 +1,13 @@
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(2000000);
+  Serial.begin(9600);
   while(!Serial){}
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   //compute random floating point number
-  float randomNumber = random(-200, 200) / 100.0;
+  int randomNumber = random(0, 100);
   Serial.print(randomNumber);
 
   delay(2000); //wait for 2 seconds
